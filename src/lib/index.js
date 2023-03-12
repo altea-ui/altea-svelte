847e182499c423d030359d933e00d517346a9368
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-03-12 14:06:24 +0100

    fix: bump to .1.2 and import relative css stylesheet

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
 // Reexport your entry components here
 import Button from './components/Button/Button.svelte';
 import Card from './components/Card/Card.svelte';
-import '$lib/styles/globals.css';
+import './styles/globals.css';
 
 // import * as Style from './components/Style.css';
 export { Button, Card };
