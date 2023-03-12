977214c0c2d55d9702c9f2abd79c936356b4310b
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-03-12 11:31:01 +0100

    fix: moved the entire page with margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,33 @@
 	}
 </script>
 
-<h1>Welcome Altea</h1>
-<p>A Work in progress Svelte based ui.</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<br/>
-<Button style="margin: 20px; width: 200px;" kind="secondary" variant="" size="large" on:click={handleClick}>Upload</Button>
-<Button style="margin: 20px; width: 200px;" kind="primary" shape="default" variant="shadow" size="large" on:click={handleClick}>Upload</Button>
-<Button style="margin: 20px; width: 200px;" kind="violet" variant="" size="large" on:click={handleClick}>Upload</Button>
+<div style="margin: 50px;">
+	<h1>Welcome Altea</h1>
+	<p>A Work in progress Svelte based ui.</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<br />
+	<Button
+		style="width: 200px;"
+		kind="secondary"
+		variant=""
+		size="large"
+		on:click={handleClick}>Upload</Button
+	>
+	<br/>
+	<Button
+		style="width: 200px;"
+		kind="primary"
+		shape="default"
+		variant="shadow"
+		size="large"
+		on:click={handleClick}>Upload</Button
+	>
+	<br/>
+	<Button
+		style="width: 200px;"
+		kind="violet"
+		variant=""
+		size="large"
+		on:click={handleClick}>Upload</Button
+	>
+</div>
