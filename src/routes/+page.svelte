14fbf69fedd93fecf992434f496330f18c5eee35
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-03-12 11:34:00 +0100

    chore: some more work on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,37 +3,38 @@
 	import Button from '$lib/components/Button/Button.svelte';
 
 	function handleClick() {
-		console.log('Button Clicked');
+		alert('Button Clicked');
 	}
 </script>
 
 <div style="margin: 50px;">
 	<h1>Welcome Altea</h1>
-	<p>A Work in progress Svelte based ui.</p>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>A Work in progress Svelte based ui library.</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
 	<br />
-	<Button
-		style="width: 200px;"
-		kind="secondary"
-		variant=""
-		size="large"
-		on:click={handleClick}>Upload</Button
-	>
-	<br/>
 	<Button
 		style="width: 200px;"
 		kind="primary"
 		shape="default"
 		variant="shadow"
 		size="large"
-		on:click={handleClick}>Upload</Button
+		on:click={handleClick}>Button</Button
 	>
+	<br/>
+	<Button
+		style="width: 200px;"
+		kind="secondary"
+		variant=""
+		size="large"
+		on:click={handleClick}>Button</Button
+		>
 	<br/>
 	<Button
 		style="width: 200px;"
 		kind="violet"
 		variant=""
 		size="large"
-		on:click={handleClick}>Upload</Button
+		on:click={handleClick}>Button</Button
 	>
+	<p><strong>TODO</strong>: Add spinner to <a href="#button">Button</a></p>
 </div>
